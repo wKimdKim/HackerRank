9faedcf7d1139be676bc6f01df3d13846300de03
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-02-19 22:24:33 +1300

    Completed Fibonacci

---
+++ HackerRank/Fibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace HackerRank
+{
+    public class FibonacciCalculator
+    {
+        public FibonacciCalculator()
+        {
+        }
+
+        public static int Calculate(int n)
+        {
+
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Calculate(n - 1) + Calculate(n - 2);
+            }
+        }
+
+        public void Run()
+        {
+                var nTerm = Convert.ToInt32(Console.ReadLine());
+                var result = FibonacciCalculator.Calculate(nTerm);
+                Console.WriteLine(result);
+
+        }
+    }
+}
