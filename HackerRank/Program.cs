172c4ae68fb9589022a94ec2b55f534cdcdb0a84
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-03-07 00:12:30 +1300

    Finished RansomNote

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -10,10 +10,13 @@ namespace HackerRank
             //test.Run();                                 // Timed out on last two
 
             Anagram test2 = new Anagram();
-            test2.Run();                                  // Full complete
+            //test2.Run();                                  // Full complete
 
             FibonacciCalculator test3 = new FibonacciCalculator();
             //test3.Run();
+
+            RansomNote test4 = new RansomNote();
+            test4.Run();
 
         }
     }
---
+++ HackerRank/RansomNote.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HackerRank
+{
+    public class RansomNote
+    {
+        public RansomNote()
+        {
+        }
+
+        public void Run()
+        {
+            var wordCount = Console.ReadLine().Split();
+            var magazineWords = Console.ReadLine().Split();
+            var ransomNote = Console.ReadLine().Split();
+            Dictionary<string, int> wordCounter = new Dictionary<string, int>();
+
+            if (Convert.ToInt32(wordCount[0]) < Convert.ToInt32(wordCount[1]))
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            foreach (string word in magazineWords)
+            {
+                if (wordCounter.ContainsKey(word) == true)
+                {
+                    wordCounter[word]++;
+                }
+                else
+                {
+                    wordCounter.Add(word, 1);
+                }
+            }
+
+            foreach (string word in ransomNote)
+            {
+                if (wordCounter.ContainsKey(word) == false)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                wordCounter[word]--;
+            }
+
+            foreach (KeyValuePair<string, int> pair in wordCounter)
+            {
+                if (pair.Value < 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+        }
+    }
+}
